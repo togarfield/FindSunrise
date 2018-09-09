68e0f542815e2fb4fde7c2e685a3873e71ba91c6
mabduhsiregar <mabduhsiregar@gmail.com>
2018-09-09 21:44:50 +0700

    First Commit

--- FindSunrise/ViewController.swift
+++ FindSunrise/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblHasil: UILabel!
+    
+    @IBOutlet weak var txtCityName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,31 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func btnFindCity(_ sender: Any) {
+        let url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22\(txtCityName.text!)%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        loadUrl(url: url)
+    }
+    
+    func loadUrl(url:String){
+        DispatchQueue.global().async {
+            do{
+                let appURL = URL(string: url)!
+                let data = try Data(contentsOf: appURL)
+                let json = try JSONSerialization.jsonObject(with: data) as! [String:Any]
+                let query = json["query"] as! [String:Any]
+                let result = query["results"] as! [String:Any]
+                let channel = result["channel"] as! [String:Any]
+                let astronomy = channel["astronomy"] as! [String:Any]
+                
+                DispatchQueue.main.async {
+                    self.lblHasil.text = "Sunrise time is \(astronomy["sunrise"]!)"
+                }
+            }catch{
+                DispatchQueue.main.async {
+                    self.lblHasil.text="cannot reach server"
+                }
+            }
+        }
+    }
 }
 
